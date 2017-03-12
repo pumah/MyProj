cc683bf8ff9c4a3dc72adb2a56a25b014dbce05f
pumah <Mahendra_131@rediffmail.com>
2017-03-12 14:53:13 +0530

    URL

--- MyProj/urls.py
+++ MyProj/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from FirstApp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^FirstApp/', include('FirstApp.urls')),
+	url(r'^$', views.index, name='index'),
 ]
